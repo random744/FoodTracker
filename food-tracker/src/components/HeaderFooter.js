36eb399d51d73f76a662ec53f8ed32e198ef7cb8
u72098 <ti-berger@outlook.com>
2024-07-17 12:37:02 +0200

    remove useless footer

--- food-tracker/src/components/HeaderFooter.js
+++ food-tracker/src/components/HeaderFooter.js
@@ -14,11 +14,6 @@ const HeaderFooter = ({ children }) => {
           {children}
         </div>
       </main>
-      <footer className="bg-gray-800 text-white py-4 mt-auto">
-        <div className="container mx-auto px-4 text-center">
-          <p>&copy; TB</p>
-        </div>
-      </footer>
     </div>
   );
 };
